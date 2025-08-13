945e530d144cf3ccd099feb7a1b4889c6336cea4
Xiaoyun Xu <xiaxu@f2r9s11.clusters.42paris.fr>
2025-08-13 21:34:41 +0200

    My heart broken

---
+++ day0/ex03/NULL_not_found.py
@@ -0,0 +1,19 @@
+def NULL_not_found(object: any) -> int:
+    if object is None:
+        print(f"Nothing: {object} {type(object)}")
+        return 0
+    elif isinstance(object, float) and object != object:
+        print(f"Cheese: {object} {type(object)}")
+        return 0
+    elif object == 0 and type(object) is int:
+        print(f"Zero: {object} {type(object)}")
+        return 0
+    elif object == '' and type(object) is str:
+        print(f"Empty: {type(object)}")
+        return 0
+    elif object is False and type(object) is bool:
+        print(f"Fake: {object} {type(object)}")
+        return 0
+    else:
+        print("Type not Found")
+        return 1
